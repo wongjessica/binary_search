7fb5f81c60ff31ca4bc4c6d0cb731bdcb726a0d6
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2019-03-11 07:10:20 -0400

    Create binary-search.cpp

---
+++ binary-search.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Binary_Search(const vector<int> &numbers, int value);
+
+int main() {
+   
+vector <int>my_numbers;
+  
+for (int i=0; i<4000000; i++) my_numbers.push_back(i);
+
+cout << "Size of vector my_nymbers :" << my_numbers.size() << endl; 
+	Binary_Search(my_numbers, 2);
+	Binary_Search(my_numbers, 23);
+	Binary_Search(my_numbers, 234);
+	Binary_Search(my_numbers, 7655);
+	Binary_Search(my_numbers, 10101);
+	Binary_Search(my_numbers, 895543);
+	Binary_Search(my_numbers, 3785111);
+
+return 0;
+}
+
+void Binary_Search(const vector< int> &my_numbers, int key) {
+
+int iteration = 0, left = 0, right = my_numbers.size()-1, mid;
+
+while (left <= right) {
+	iteration++;
+	mid = (int) ((left + right) / 2);
+	if (key == my_numbers[mid]) {
+		cout << "Binary search found " << my_numbers[mid] << " after " << iteration << " iterations.\n";
+		iteration++;
+		return;
+	}
+else if (key > my_numbers[mid])
+	left = mid + 1;
+else
+	right = mid - 1;
+}
+cout << "Binary search did not found " << my_numbers[mid] << " after " << iteration << " iterations.\n";
+
+return;
+
+}
